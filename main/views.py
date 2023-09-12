99cf31d33b5ff5cc57a1b894ca250c6576b10dbb
NadhiraWidyaniswari <nadhira.wiedyakusumo@gmail.com>
2023-09-12 19:03:58 +0700

    mengubah kode main.html

--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from .models import Product
 
 def show_main(request):
     context = {
@@ -9,9 +8,7 @@ def show_main(request):
         'date_added' : '09/09/2023',
         'amount': 50000,
         'description' : 'pelembab wajah',
-        'rating' : "4.9/5.0"
 
     }
     return render(request, "main.html", context)
 
-skintipic = Product(name="skintipic", date_added =10/12/2023, price=5000, description ="Produk kecantikan", rating="4.0/5.0")
