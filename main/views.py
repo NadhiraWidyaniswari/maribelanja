cbcb2473704f9eeb3bafe98da978392380d7c4af
NadhiraWidyaniswari <nadhira.wiedyakusumo@gmail.com>
2023-09-12 18:55:02 +0700

    menambahkan tulisan dalam main.html

--- main/views.py
+++ main/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 def show_main(request):
     context = {
         'name': 'Nadhira Widyaniswari',
-        'class': 'PBP E'
-    }
+        'class': 'PBP E',
+        'product_name': 'skintipic',
+        'date_added' : '09/09/2023',
+        'amount': 50000,
+        'description' : 'pelembab wajah',
+        'rating' : "4.9/5.0"
 
+    }
     return render(request, "main.html", context)
-# Create your views here.
+
+skintipic = Product(name="skintipic", date_added =10/12/2023, price=5000, description ="Produk kecantikan", rating="4.0/5.0")
